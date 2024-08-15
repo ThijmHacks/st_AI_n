1d42975043dcb904b2daf76c59fc9f22bf9a17a3
ThijmHacks <thijmen.theloosen@outlook.com>
2024-08-15 23:09:28 +0200

    First push of the project!

---
+++ python/main.py
@@ -0,0 +1,29 @@
+import os
+import random
+import time
+from http.client import responses
+
+import st_AI_n.wait
+import st_AI_n.lazyness
+import st_AI_n.st_AI_n__PLUS
+import st_AI_n.lists
+import st_AI_n.responses
+
+os.system("cls")
+
+while True:
+    print("If you need help with anything feel free to ask.")
+
+    prompt = input("Your prompt: ")
+
+    #Waiting for response
+    totaldots = random.randint(1,10) #1,60
+    timebetween = random.randint(1,1) / 10 #1,4 /4
+    st_AI_n.wait.wait(totaldots,timebetween)
+
+    #The responce
+    st_AI_n.responses.response(prompt)
+    time.sleep(5)
+
+    #AI needs a break
+    st_AI_n.st_AI_n__PLUS.proptslimiteddef()
