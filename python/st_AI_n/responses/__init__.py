fc6805b6f851171391aa51387b6552e3ddff9eb3
ThijmHacks <thijmen.theloosen@outlook.com>
2024-08-16 21:49:43 +0200

    Added very normal brownies.py

--- python/st_AI_n/responses/__init__.py
+++ python/st_AI_n/responses/__init__.py
@@ -1,10 +1,12 @@
 import st_AI_n.lists
 from st_AI_n.lists import randomunknownprompt
-
+from st_AI_n.devil_st_AI_n.brownies import very_normal_brownies
 
 def response(prompt):
     randomanswervar = st_AI_n.lists.checkindir(prompt)
 
     if randomanswervar is not None:
-        print(st_AI_n.lists.randomanswer(randomanswervar))
+        if randomanswervar <= 99:
+            print(st_AI_n.lists.randomanswer(randomanswervar))
+        elif randomanswervar == 100: very_normal_brownies()
     else: print(randomunknownprompt())
