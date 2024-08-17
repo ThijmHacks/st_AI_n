73ce49dccd1285e8294f57f453e643a560a9ad1a
sam <hijmanssam@gmail.com>
2024-08-17 17:03:17 +0200

    added number guessing game

--- python/st_AI_n/responses/__init__.py
+++ python/st_AI_n/responses/__init__.py
@@ -1,6 +1,7 @@
 import st_AI_n.lists
 from st_AI_n.lists import randomunknownprompt
 from st_AI_n.devil_st_AI_n.brownies import very_normal_brownies
+from st_AI_n.games.guess_number import guess_number
 
 def response(prompt):
     randomanswervar = st_AI_n.lists.checkindir(prompt)
@@ -8,5 +9,9 @@ def response(prompt):
     if randomanswervar is not None:
         if randomanswervar <= 99:
             return  st_AI_n.lists.randomanswer(randomanswervar)
-        elif randomanswervar == 100: very_normal_brownies()
-    else: return randomunknownprompt()
+        elif randomanswervar == 100:
+            very_normal_brownies()
+        elif randomanswervar == 200:
+            guess_number()
+    else:
+        return randomunknownprompt()
